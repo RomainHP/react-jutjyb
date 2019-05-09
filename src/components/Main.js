6a32dc71de9353866b7db559fb88a8f69e1957db
HotPotatoes86 <rcharpentier10@gmail.com>
2019-05-09 10:31:20 +0200

    4 files modified, 1 added

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,7 @@
 import React,  { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
+import Topbar from './Topbar';
 
 class Main extends Component {
 
@@ -8,7 +9,7 @@ class Main extends Component {
     const { classes } = this.props;
     return (
       <React.Fragment>
-        
+        <Topbar />
       </React.Fragment>
     )
   }

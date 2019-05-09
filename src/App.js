e692fa846828e28680eccfaa29eff75f03f51256
HotPotatoes86 <rcharpentier10@gmail.com>
2019-05-09 09:44:43 +0200

    2 files modified

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import './App.css';
+import { blue, indigo } from '@material-ui/core/colors'
+
+const theme = createMuiTheme({
+  palette: {
+    secondary: {
+      main: blue[900]
+    },
+    primary: {
+      main: indigo[700]
+    }
+  },
+  typography: {
+    // Use the system font instead of the default Roboto font.
+    fontFamily: [
+      '"Lato"',
+      'sans-serif'
+    ].join(',')
+  }
+});
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <MuiThemeProvider theme={theme}>
+          <Routes />
+        </MuiThemeProvider>
+      </div>
+    );
+  }
+}

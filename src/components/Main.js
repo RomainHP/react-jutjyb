ed0fa61c444be7f0edd7ee44d6ad827e240d22ad
HotPotatoes86 <rcharpentier10@gmail.com>
2019-05-10 16:31:08 +0200

    1 file modified, 1 added

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,84 @@
 import React,  { Component } from 'react';
 import { withRouter } from 'react-router-dom';
-import Button from '@material-ui/core/Button';
 import Topbar from './Topbar';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import IconButton from '@material-ui/core/IconButton';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+import Typography from '@material-ui/core/Typography';
+import tileData from './tileData';
+
+const styles = theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
+  },
+  gridList: {
+    width: '95%',
+    height: '95%',
+    // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+    transform: 'translateZ(0)',
+  },
+  titleBar: {
+    background:
+      'linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, ' +
+      'rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+  },
+  icon: {
+    color: 'white',
+  },
+});
 
 class Main extends Component {
-
+  
   render() {
     const { classes } = this.props;
     return (
       <React.Fragment>
         <Topbar />
+        <p />
+        <Grid container spacing={24} className={classes.grid}>
+          <Grid item xs={6} className={classes.griditem}>
+            <div className={classes.root}>
+              <GridList cellHeight={200} spacing={1} className={classes.gridList}>
+                {tileData.map(tile => (
+                  <GridListTile key={tile.img} cols={tile.featured ? 2 : 1} rows={tile.featured ? 2 : 1}>
+                    <img src={tile.img} alt={tile.title} />
+                    <GridListTileBar
+                      title={tile.title}
+                      subtitle={tile.author}
+                      titlePosition="top"
+                      actionIcon={
+                        <IconButton className={classes.icon}>
+                          <StarBorderIcon />
+                        </IconButton>
+                      }
+                      actionPosition="left"
+                      className={classes.titleBar}
+                    />
+                  </GridListTile>
+                ))}
+              </GridList>
+            </div>
+          </Grid>
+          <Grid item xs={6} className={classes.griditem}>
+            <Typography variant='h1' align='center' color='text.primary'>Title</Typography>
+            <p />
+            <Typography style={{width:'95%', height:'95%', textAlign:'justify'}}>
+              Quod opera consulta cogitabatur astute, ut hoc insidiarum genere Galli periret avunculus, ne eum ut praepotens acueret in fiduciam exitiosa coeptantem. verum navata est opera diligens hocque dilato Eusebius praepositus cubiculi missus est Cabillona aurum secum perferens, quo per turbulentos seditionum concitores occultius distributo et tumor consenuit militum et salus est in tuto locata praefecti. deinde cibo abunde perlato castra die praedicto sunt mota. Nisi mihi Phaedrum, inquam, tu mentitum aut Zenonem putas, quorum utrumque audivi, cum mihi nihil sane praeter sedulitatem probarent, omnes mihi Epicuri sententiae satis notae sunt. atque eos, quos nominavi, cum Attico nostro frequenter audivi, cum miraretur ille quidem utrumque, Phaedrum autem etiam amaret, cotidieque inter nos ea, quae audiebamus, conferebamus, neque erat umquam controversia, quid ego intellegerem, sed quid probarem. Et quia Mesopotamiae tractus omnes crebro inquietari sueti praetenturis et stationibus servabantur agrariis, laevorsum flexo itinere Osdroenae subsederat extimas partes, novum parumque aliquando temptatum commentum adgressus. quod si impetrasset, fulminis modo cuncta vastarat. erat autem quod cogitabat huius modi. Novo denique perniciosoque exemplo idem Gallus ausus est inire flagitium grave, quod Romae cum ultimo dedecore temptasse aliquando dicitur Gallienus, et adhibitis paucis clam ferro succinctis vesperi per tabernas palabatur et conpita quaeritando Graeco sermone, cuius erat inpendio gnarus, quid de Caesare quisque sentiret. et haec confidenter agebat in urbe ubi pernoctantium luminum claritudo dierum solet imitari fulgorem. postremo agnitus saepe iamque, si prodisset, conspicuum se fore contemplans, non nisi luce palam egrediens ad agenda quae putabat seria cernebatur. et haec quidem medullitus multis gementibus agebantur.
+              His cognitis Gallus ut serpens adpetitus telo vel saxo iamque spes extremas opperiens et succurrens saluti suae quavis ratione colligi omnes iussit armatos et cum starent attoniti, districta dentium acie stridens adeste inquit viri fortes mihi periclitanti vobiscum.
+            </Typography>
+          </Grid>
+        </Grid>
       </React.Fragment>
     )
   }
 }
 
-export default withRouter(Main);
+export default withRouter(withStyles(styles)(Main));
---
+++ src/components/tileData.js
@@ -0,0 +1,20 @@
+const tileData = [
+  {
+    img: 'https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    title: 'Paysage',
+    author: 'Inconnu',
+    featured: true,
+  },
+  {
+    img: 'https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/_jcr_content/main-pars/image/visual-reverse-image-search-v2_1000x560.jpg',
+    title: 'Papillon',
+    author: 'Inconnu',
+  },
+  {
+    img: 'https://i.pinimg.com/originals/ac/e8/49/ace849f4cca62e9914256c3f9059cdd8.jpg',
+    title: 'Portal',
+    author: 'Inconnu',
+  },
+];
+
+export default tileData;
